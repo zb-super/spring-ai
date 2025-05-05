bde12ca333a42e2d3e8c786cab9e9fe88f6b2263
bz <zhang_bin3@venusgroup.com.cn>
2025-05-05 23:13:16 +0800

    调整pom依赖

--- spring-ai-reactor/src/main/java/com/zb/reactor/Main.java
+++ spring-ai-reactor/src/main/java/com/zb/reactor/Main.java
@@ -1,8 +1,11 @@
 package com.zb.reactor;
 
+import reactor.core.publisher.Flux;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Flux.range(1,10)
+                .subscribe(System.out::println);
     }
 }
