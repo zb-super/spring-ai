816919e9eb35f34ac6d4a7ce67323e83ad47e977
张彬 <1138246411@qq.com>
2025-05-06 22:13:46 +0800

    reactor学习笔记

---
+++ spring-ai-reactor/src/main/java/com/zb/reactor/demo/ContractDemo.java
@@ -0,0 +1,22 @@
+package com.zb.reactor.demo;
+
+import reactor.core.publisher.Flux;
+
+/**
+ * Description
+ * Copyright © 启明星辰 版权所有
+ *
+ * @author：binzhang
+ * @date： 2025/5/6
+ */
+public class ContractDemo {
+    public static void main(String[] args) {
+        Flux.just(1, 3, 4)
+                .concatWith(Flux.just(4, 5, 6, 7, 8))
+                .map(item ->  item*2)
+                .filter(item -> item > 14)
+                .log()
+                .subscribe(System.out::println);
+
+    }
+}
---
+++ spring-ai-reactor/src/main/java/com/zb/reactor/demo/SubscribeDemo.java
@@ -0,0 +1,20 @@
+package com.zb.reactor.demo;
+
+import reactor.core.publisher.Flux;
+
+/**
+ * Description
+ * Copyright © 启明星辰 版权所有
+ *
+ * @author：binzhang
+ * @date： 2025/5/6
+ */
+public class SubscribeDemo {
+    public static void main(String[] args) {
+        Flux.just(1, 3, 4,5,7,8,9,0)
+                .subscribe(System.out::println,
+                        er -> System.out.println(er.getMessage()),
+                        () -> System.out.println("complete"),
+                        subscription -> subscription.request(Long.MAX_VALUE));
+    }
+}
