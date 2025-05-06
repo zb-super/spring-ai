7cb370b215aef855aabdb7cf85391365988103e5
bz <zhang_bin3@venusgroup.com.cn>
2025-05-06 12:43:39 +0800

    事件感知函数

---
+++ spring-ai-reactor/src/main/java/com/zb/reactor/demo/TwoSubscribe.java
@@ -0,0 +1,15 @@
+package com.zb.reactor.demo;
+
+import reactor.core.publisher.Flux;
+
+public class TwoSubscribe {
+    public static void main(String[] args) {
+        Flux<Integer> range = Flux.range(1, 10);
+        range.subscribe(item ->{
+            System.out.println("线程名称：" + Thread.currentThread().getName() + "值" +item);
+        });
+        range.subscribe(item ->{
+            System.out.println("线程名称：" + Thread.currentThread().getName() + "值" +item);
+        });
+    }
+}
