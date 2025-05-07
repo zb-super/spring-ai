17a60b8213c57b5775a76a8ead7062d62c2675b1
张彬 <1138246411@qq.com>
2025-05-07 22:14:59 +0800

    reactor学习笔记

---
+++ spring-ai-reactor/src/main/java/com/zb/reactor/demo/LimitRateDemo.java
@@ -0,0 +1,20 @@
+package com.zb.reactor.demo;
+
+import reactor.core.publisher.Flux;
+
+/**
+ * Description
+ * Copyright © 启明星辰 版权所有
+ *
+ * @author：binzhang
+ * @date： 2025/5/7
+ */
+public class LimitRateDemo {
+
+    public static void main(String[] args) {
+        Flux.range(1, 100)
+                .log()
+                .limitRate(10)
+                .subscribe();
+    }
+}
