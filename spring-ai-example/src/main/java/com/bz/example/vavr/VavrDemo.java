2eac6e2132e45b13d2210495c846a83a4bf1b408
张彬 <1138246411@qq.com>
2025-05-13 22:41:22 +0800

    调整项目结构

--- spring-ai-example/src/main/java/com/bz/example/vavr/VavrDemo.java
+++ spring-ai-example/src/main/java/com/bz/example/vavr/VavrDemo.java
@@ -1,16 +1,55 @@
 package com.bz.example.vavr;
 
+
 import io.vavr.Tuple;
 import io.vavr.Tuple2;
 import io.vavr.collection.List;
+import io.vavr.collection.Queue;
+import io.vavr.collection.Stream;
 import io.vavr.control.Option;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class VavrDemo {
 
     public static void main(String[] args) {
-        List<String> append = List.of("1").append("2");
-        Tuple2<Integer, Integer> integerIntegerTuple2 = Tuple.of(1, 2);
-        Option.of(null);
-//        Try.of()
+
+        VavrDemo.testStream();
     }
+
+    private void testList(){
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6, 7).append(8);
+        for (Integer integer : integers) {
+
+        }
+
+    }
+
+    private static void testQueue(){
+        Queue<Integer> queue = Queue.of(1, 2, 3);
+        Tuple2<Integer, Queue<Integer>> dequeued = queue.dequeue();
+
+
+        Option<Tuple2<Integer, Queue<Integer>>> tuple2s = Queue.of(1).dequeueOption();
+
+        Option<Tuple2<Object, Queue<Object>>> tuple2s1 = Queue.empty().dequeueOption();
+        System.out.println(tuple2s1.isEmpty());
+//        Tuple2<Object, Queue<Object>> dequeue = Queue.empty().dequeue();
+
+        Option<Integer> map = tuple2s.map(item -> item._1);
+        Option<Queue<Integer>> map1 = tuple2s.map(item -> item._2);
+
+        System.out.println(1);
+    }
+
+    private static void testStream(){
+        // = Stream("1", "2", "3") 在 Vavr 中
+        Stream.of(1, 2, 3).map(Object::toString).forEach(System.out::println);
+    }
+
+    private void testTuple(){
+//        Tuple.
+    }
+
 }
