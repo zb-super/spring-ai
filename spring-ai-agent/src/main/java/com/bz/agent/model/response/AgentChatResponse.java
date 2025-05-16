56d54db675b6e7026a29cea54777bc1b20dc22a3
bz <zhang_bin3@venusgroup.com.cn>
2025-05-16 14:37:58 +0800

    feature:增加消息类型

--- spring-ai-agent/src/main/java/com/bz/agent/model/response/AgentChatResponse.java
+++ spring-ai-agent/src/main/java/com/bz/agent/model/response/AgentChatResponse.java
@@ -21,10 +21,33 @@ public interface AgentChatResponse {
     }
 
     enum Type {
+        /**
+         * 思考内容
+         */
         THINK,
+        /**
+         * 调用工具的消息
+         */
         TOOL_BEFORE,
+        /**
+         * 工具的返回结果
+         */
         TOOL_RESULT,
+        /**
+         * 报错结果
+         */
         ERROR,
-        TEXT;
+        /**
+         * 大模型回答的正文
+         */
+        TEXT,
+        /**
+         * 各类消息，分隔符号
+         */
+        DELIMITER,
+        /**
+         * 停止信号
+         */
+        STOP
     }
 }
---
+++ spring-ai-agent/src/main/java/com/bz/agent/model/response/DelimiterChatResponse.java
@@ -0,0 +1,18 @@
+package com.bz.agent.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DelimiterChatResponse implements AgentChatResponse {
+
+    private Object data;
+
+    @Override
+    public Type getType() {
+        return Type.DELIMITER;
+    }
+}
