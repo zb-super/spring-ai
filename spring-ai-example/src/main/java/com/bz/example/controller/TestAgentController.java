f973cffea8ec37e837ebd08d5fa926eb9c9ad418
张彬 <1138246411@qq.com>
2025-05-17 22:16:22 +0800

    feature:智能体1.1

--- spring-ai-example/src/main/java/com/bz/example/controller/TestAgentController.java
+++ spring-ai-example/src/main/java/com/bz/example/controller/TestAgentController.java
@@ -32,7 +32,7 @@ public class TestAgentController {
 
     @GetMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     Flux<AgentChatResponse> generation(@RequestParam("userInput") String userInput) {
-        String key = "sk-0f38ec133e2c466089d90e7fc18fafcc";
+        String key = "";
         User user = User.builder()
                 .userInput(userInput)
                 .build();
@@ -57,46 +57,4 @@ public class TestAgentController {
         return agentExecutor.chatStream(context)
                 .filter(item -> item.getData() != null);
     }
-
-    @GetMapping(value = "/chatV2", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    Flux<ServerSentEvent<String>> generationV2(@RequestParam("userInput") String userInput) {
-        String key = "sk-0f38ec133e2c466089d90e7fc18fafcc";
-        User user = User.builder()
-                .userInput(userInput)
-                .build();
-
-        ChatOptions chatOptions = ChatOptions.builder()
-                .apiKey(key)
-                .baseUrl("https://dashscope.aliyuncs.com/compatible-mode")
-                .model("qwq-plus")
-                .maxToken(1024)
-                .build();
-
-        AgentExecutor agentExecutor = new QwenAgentExecutor();
-        ChatContext context = ChatContext.builder()
-                .user(user)
-                .chatOptions(chatOptions)
-                .prompt("你是一个智能助手。")
-                .messages(new ArrayList<>())
-                .knowledgeBases(new ArrayList<>())
-                .callbacks(ToolCallbacks.from(new TestUtils()))
-                .build();
-
-        Flux<ServerSentEvent<String>> log = agentExecutor.chatStream(context)
-                .filter(item -> item.getData() != null)
-                .map(item -> ServerSentEvent.builder(item.getData().toString()).build())
-
-                .log();
-
-        return log;
-    }
-
-    @GetMapping(value = "/chatV3", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    Flux<Integer> generationV3(@RequestParam("userInput") String userInput) {
-        return Flux.range(1,10)
-                .delayElements(Duration.ofSeconds(5));
-
-    }
-
-
 }
