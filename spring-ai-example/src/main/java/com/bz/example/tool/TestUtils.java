63d7020c9ac1c4410c4549f8062a018906441ef7
张彬 <1138246411@qq.com>
2025-05-18 18:20:07 +0800

    feature:智能体1.3

--- spring-ai-example/src/main/java/com/bz/example/tool/TestUtils.java
+++ spring-ai-example/src/main/java/com/bz/example/tool/TestUtils.java
@@ -5,6 +5,7 @@ import org.springframework.ai.tool.annotation.ToolParam;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Map;
 
 /**
  * Description
@@ -26,6 +27,11 @@ public class TestUtils {
     @Tool(description = "获取天气预报")
     String getWeather(@ToolParam(description = "时间") String time){
         return "雨夹雪";
+    }
+
+    @Tool(description = "查询用户信息", returnDirect = true)
+    Map<String, Object> findUserInfo(@ToolParam(description = "名字") String name){
+        return Map.of("name","zb","age",12);
     }
 
 }
