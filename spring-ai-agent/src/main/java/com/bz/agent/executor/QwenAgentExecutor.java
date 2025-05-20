f52d468c62361d42d2ee31934015d5392b6dbd30
bz <zhang_bin3@venusgroup.com.cn>
2025-05-20 17:08:56 +0800

    feature:执行器模板方法代码调整

--- spring-ai-agent/src/main/java/com/bz/agent/executor/QwenAgentExecutor.java
+++ spring-ai-agent/src/main/java/com/bz/agent/executor/QwenAgentExecutor.java
@@ -1,7 +1,6 @@
 package com.bz.agent.executor;
 
 import com.bz.agent.model.agent.AgentContext;
-import com.bz.agent.model.chat.ModelResponse;
 import com.bz.agent.model.response.AgentChatResponse;
 import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationHandler;
@@ -27,35 +26,6 @@ import java.util.List;
 
 public class QwenAgentExecutor extends AbstractAgentExecutor implements AgentExecutor {
 
-    @Override
-    protected void doChatSteam(ExecutorContext context, FluxSink<AgentChatResponse> emitter){
-        context.getChatModel()
-                .stream(context.getPrompt())
-                .doOnNext(chatResponse -> AgentExecutorUtils.sendThinkMsg(context.getIndex(), chatResponse, emitter))
-                .collectList()
-                .subscribe(chatResponseList -> {
-                    //处理模型返回的结果
-                    ModelResponse modelResponse = AgentExecutorUtils.processChatResponse(chatResponseList);
-                    // 1.是否需要执行工具
-
-                    // 2.是否需要查询知识库
-
-                    // 3.是否需要调用工作流
-
-                    // 4.直接返回给模型
-
-                    // 1.是否需要执行工具
-//                    if (modelResponse.isNeedExecTool()) {
-//                        processFunctionCall(modelResponse.getToolList(), context, emitter);
-//                    }else {
-//
-//                        //4.直接返回给模型
-//                        emitter.next(AgentChatResponse.ofTextResponse(modelResponse.toString(), context.getIndex()));
-//                        // 停止符号
-//                        emitter.next(AgentChatResponse.ofStopResponse(context.getIndex()));
-//                    }
-                });
-    }
 
     private void processFunctionCall(List<ChatResponse> toolList,
                                      ExecutorContext context,
